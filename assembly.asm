b4e30ded8cdc46b92251990ce8639923a0459c80
GabrielFeng <45602417+GabrielFeng@users.noreply.github.com>
2023-11-11 14:12:13 -0500

    Update with scale to make accurate to degree turn

--- assembly.asm
+++ assembly.asm
@@ -1,13 +1,13 @@
 ; Infinite loop where the customers program can be written
-; Every 10 is 1 ms pulse exp. 15 is 1.5 ms pulse for zero position
+; Every value inputed to servo must be scaled by 8/9 exp. 180 *(8/9) = 160. We out 160 to servo to get 180 degree turn
 ORG 0
 Loop:
-	LOADI 90
-	CALL DegreeTurn
-    CALL Delay
+    LOADI 90
+    CALL DegreeTurn
+    ;CALL Delay
     LOADI 180
     CALL DegreeTurn
-    CALL Delay
+    ;CALL Delay
     LOADI 0
     CALL DegreeTurn
     
@@ -16,6 +16,7 @@ DONE:
 
 ; @AC should contain the amount of degrees user wants to turn with respect to origin
 DegreeTurn:
+	CALL Scale
 	OUT Servo
     Return
 	
@@ -31,11 +32,13 @@ SetZero:
 ; @return the pulse in ms should be in AC once returned
 SetMax:
 	LOADI 180
+    CALL Scale
     OUT Servo
     Return
     
  SetMid:
  	LOADI 90
+    CALL Scale
     OUT Servo
     Return
 
@@ -51,6 +54,37 @@ WaitingLoop:
 	ADDI   -20 ; wait 2 seconds
 	JNEG   WaitingLoop
 	RETURN
+Scale:
+	STORE Value
+	SHIFT 3 ;x8
+    CALL DIVIDE_NINE
+    Return
+    
+; Divide by 9 function
+DIVIDE_NINE:
+	STORE   DIVIDEND
+DIVIDE_LOOP:
+    LOAD    DIVIDEND ; Load the dividend from memory
+    SUB     Nine  ; Subtract the divisor (9) from the dividend
+    STORE   DIVIDEND ; Store the updated dividend
+    JNEG    DIVIDE_DONE ; If AC < 0, we are done
+
+    LOAD    QUOTIENT ; Load the quotient from memory
+    ADDI    1       ; Increment the quotient
+    STORE   QUOTIENT ; Store the updated quotient
+    JUMP    DIVIDE_LOOP ; Repeat the division loop
+
+DIVIDE_DONE:
+    LOAD    QUOTIENT ; Load the final quotient
+    RETURN
+    
+
+; Constants
+ORG 50
+Nine: DW 9
+QUOTIENT: DW 0
+DIVIDEND: DW 0
+Value: DW 0
 
 
 ; IO address constants
