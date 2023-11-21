87956e78f913fbbe7038ca1cebbf6472306344cf
Sauerboy <130418707+Sauerboy@users.noreply.github.com>
2023-11-21 15:08:32 -0500

    sorta works

--- HSPG.vhd
+++ HSPG.vhd
@@ -24,13 +24,12 @@ end HSPG;
 architecture a of HSPG is
 
     signal command : std_logic_vector(7 downto 0);  -- command sent from SCOMP
-	 signal old_command : std_logic_vector(7 downto 0);  -- command sent from SCOMP
 	 signal degreeCommand : std_logic_vector(7 downto 0);  -- command sent from SCOMP
     signal count   : std_logic_vector(11 downto 0);  -- internal counter
 	 signal mode : std_logic;
 	 CONSTANT half_freq   : INTEGER := 90000/2;
 	 signal speed_clock : std_logic;
-	 SIGNAL count_speed_clock        : INTEGER RANGE 0 TO half_freq/360; 
+	 SIGNAL count_speed_clock        : INTEGER RANGE 0 TO half_freq; 
 	 signal updown : std_logic;
 
 begin
@@ -43,25 +42,18 @@ begin
             command <= IO_DATA(8 downto 1);
 				mode <= IO_DATA(0);
         end if;
-		  if falling_edge(CS) then
-		  old_command <= command;
-		  end if;
     end process;
 	 
 	 
-	PROCESS(CLOCk, command, old_command)
+	PROCESS(CLOCk, command)
 	BEGIN
-	IF (old_command /= command) then
-	count_speed_clock <= 0;
-	else 
 	if RISING_EDGE(CLOCK) then
-	IF count_speed_clock < (half_freq/ to_integer(IEEE.numeric_std.unsigned(command - 1))) THEN
+	IF count_speed_clock < (half_freq/ (to_integer(IEEE.numeric_std.unsigned(command + x"01")))) THEN
 			count_speed_clock <= count_speed_clock + 1;
 		ELSE
 			count_speed_clock <= 0;
 			speed_clock <= NOT(speed_clock);
 		END IF;
-	end if;
 	end if;
 	end process;
 	
@@ -73,17 +65,13 @@ begin
 		when '0' => degreeCommand <= command;
 		when '1' => 
 		if rising_edge(speed_clock) then
-		
 			if (updown = '1') then
-			degreeCommand <= degreeCommand + 1;
-			if degreeCommand = x"b4" then
-			updown <= '0';
-			end if;
+			degreeCommand <= degreeCommand + x"01";
 			else 
-			degreeCommand <= degreeCommand - 1;
-			if degreeCommand = x"00" then
-			updown <= '1';
+			degreeCommand <= degreeCommand - x"01";
 			end if;
+			if (degreeCommand >= 180 or degreeCommand <= 0) then
+			upDown <= NOT(upDown);
 			end if;
 			end if;
 		end case;
